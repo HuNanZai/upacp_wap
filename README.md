e6ce1104cfefc6cea363dc8ca8fc66da23834fe7
HuNanZai <123165587@qq.com>
2016-03-21 14:20:35 +0800

    提交基本文件

---
+++ README.md
@@ -0,0 +1,9 @@
+#银联手机网关支付组件
+
+#文档索引
+1. [银联手机开发规范][1]
+
+#开发进度
+1. 开始搭建基本框架
+
+[1]: https://open.unionpay.com/upload/download/%E6%89%8B%E6%9C%BA%E7%BD%91%E9%A1%B5%E6%94%AF%E4%BB%98%E4%BA%A7%E5%93%81%E6%8E%A5%E5%8F%A3%E8%A7%84%E8%8C%83v1.7.pdf
---
+++ src/Config.php
@@ -0,0 +1,12 @@
+<?php
+namespace HuNanZai\Component\Pay\Package\Upacp_wap;
+
+class Config
+{
+    public $sign_cert_path      = '';   //签名证书路径
+    public $sign_cert_passwd    = '';   //签名证书密码
+
+    public $encrypt_cert_path   = '';   //签名加密证书
+    public $verify_cert_dir     = '';   //验签证书路径
+
+}
